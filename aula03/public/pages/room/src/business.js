6663bf90f0da3fd1602fb11510ffdf7152a31965
HallexCosta <hallex.costa@hotmail.com>
2021-01-31 02:06:39 -0300

    Add aula03
    Signed-off-by: HallexCosta <hallex.costa@hotmail.com>

---
+++ aula03/public/pages/room/src/business.js
@@ -0,0 +1,150 @@
+class Business {
+  constructor({ room, media, view, socketBuilder, peerBuilder }) {
+    Object.assign(this, { room, media, view, socketBuilder, peerBuilder });
+      
+    this.currentStream = {}
+    this.socket = {}
+    this.currentPeer = {}
+
+    this.peers = new Map()
+    this.usersRecordings = new Map()
+  }
+
+  static initialize(deps) {
+    const instance = new Business(deps)
+    return instance._init()
+  }
+
+  async _init() {
+    this.view.configureRecordButton(this.onRecordPressed.bind(this))
+
+    this.currentStream = await this.media.getCamera()
+
+    this.socket = await this.socketBuilder
+      .setOnUserConnected(this.onUserConnected())
+      .setOnUserDisconnected(this.onUserDisconnected())
+      .build()
+      
+
+    this.currentPeer = await this.peerBuilder
+      .setOnError(this.onPeerError())
+      .setOnConnectionOpened(this.onConnectionOpened())
+      .setOnCallReceived(this.onPeerCallReceived())
+      .setOnPeerStreamReceived(this.onPeerStreamReceived())
+      .setOnCallError(this.onPeerCallError())
+      .setOnCallClose(this.onPeerCallClose())
+      .build()
+
+    this.addVideoStream(this.currentPeer.id)
+  }
+  
+  addVideoStream(userId, stream = this.currentStream) {
+    const recorderInstance = new Recorder(userId, stream)
+    this.usersRecordings.set(recorderInstance.filename, recorderInstance)
+
+    if (this.recordingEnabled) {
+      recorderInstance.startRecording()
+    }
+
+    const isCurrentId = false
+    this.view.renderVideo({
+      userId,
+      stream, 
+      isCurrentId
+    })
+  }
+
+  onUserConnected() {
+    return userId => {
+      console.log('User connected!', userId)
+      this.currentPeer.call(userId, this.currentStream)
+    }
+  }
+
+  onUserDisconnected() {
+    return userId => {
+      console.log('User disconnected!', userId)
+
+      if (this.peers.has(userId)) {
+        this.peers.get(userId).call.close()
+        this.peers.delete(userId)
+      }
+
+      this.view.setParticipants(this.peers.size)
+      this.view.removeVideoElement(userId)
+    }
+  }
+
+  onPeerError() {
+    return error => {
+      console.error('error on peer!', error)
+    }
+  }
+
+  onConnectionOpened() {
+    return peer => {
+      const id = peer.id
+      console.log('peer!!', peer)
+      this.socket.emit('join-room', this.room, id)
+    }
+  }
+
+  onPeerCallReceived() {
+    return call => {
+      console.log('answering call', call)
+      call.answer(this.currentStream)
+    }
+  }
+
+  onPeerStreamReceived() {
+    return (call, stream) => {
+      const callerId = call.peer
+      this.addVideoStream(callerId, stream)
+      this.peers.set(callerId, { call })
+
+      this.view.setParticipants(this.peers.size)
+    }
+  }
+
+  onPeerCallError() {
+    return (call, error) => {
+      console.log('an call error ocurred!', error)
+      this.view.removeVideoElement(call.peer)
+    }
+  }
+
+  onPeerCallClose() {
+    return call => {
+      console.log('call closed!!', call.peer)
+    }
+  }
+
+  onRecordPressed(recordingEnabled) {
+    this.recordingEnabled = recordingEnabled
+    console.log('pressionou!!', recordingEnabled)
+    for (const [key, value] of this.usersRecordings) {
+      if (this.recordingEnabled) {
+        value.startRecording()
+        continue;
+      }
+      this.stopRecording(key)
+    }
+  }
+
+  // Se o usuário entrar e sair da call durante uma gravação
+  // precisamos parar as gravações anteriores dele
+  async stopRecording(userId) {
+    const userRecordings = this.usersRecordings
+    for (const [key, value] of userRecordings) {
+      const isContextUser = key.includes(userId)
+      if (!isContextUser) continue;
+
+      const rec = value
+      const isRecordingActive = rec.recordingActive
+
+      if (!isRecordingActive) continue;
+
+      await rec.stopRecording()
+    }
+  }
+}
